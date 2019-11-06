11b59f85b3b6958f8412c2c6b2f59a6a714ecb3b
Jalihal <amogh.jalihal@gmail.com>
2019-11-06 11:52:10 -0500

    Update .gitignore, rename obsolete simulator.py

--- nutrient_signaling/simulator_old.py
+++ nutrient_signaling/simulator_old.py
@@ -1,4 +1,8 @@
 """Collection of utilities for simulating and analyzing model
+
+TODO: The functions here are outdated. The functionality has been moved
+to other locations where suitable. I am keeping this around for reference
+mainly for myself. Here be bad bad code.
 """
 
 import PyDSTool as dst
