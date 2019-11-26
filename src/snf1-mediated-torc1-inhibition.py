0f71a3f984aa0a410e9a1a4c4b84fa147e950a79
Jalihal <amogh.jalihal@gmail.com>
2019-11-26 11:46:54 -0500

    Refactored script to generate Snf1-TORC1 inhibition hypothesis

--- src/snf1-mediated-torc1-inhibition.py
+++ src/snf1-mediated-torc1-inhibition.py
@@ -1,79 +1,88 @@
 import os
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
-HOME = os.path.expanduser('~')
-os.chdir(HOME + '/jalihal_projects/Research/data/ModelAnalysis/')
-import modelreader as md
-import simulator as sim
+# Local Imports
+from nutrient_signaling.simulators import get_simulator
+font = {'size'   : 16}
+matplotlib.rc('font', **font)
 
-Model = md.main('./ParameterSets/2018-9-26-12-3-no-sigma/')
-f, ax = plt.subplots(1,2,figsize=(8,4))
+f, ax = plt.subplots(1,2, figsize=(10,5))
+LW = 4
+
 ## Prouteau, 2017
-pre = {'parameters':{'Carbon':1.0,'ATP':1.0,'Glutamine_ext':1.0},'inconds':{}}
-post = {'parameters':{'Carbon':0.0,'ATP':0.0,'Glutamine_ext':1.0},'inconds':{}}
-DS = sim.createModelObject(Model)
-DS_mut = sim.createModelObject(Model)
-DS_mut.set(pars={'w_torc_snf':0.0})
+ax[0].plot([0.0,2.5,5.0,15.0,30.0],
+           [1.0,0.34,0.13,0.15,0.20],
+           'ko',markersize=8)
+ax[1].plot([0.0,2.0,5.0,15.0,30.0],
+           [0.20,0.63,0.92,0.91,1.0],
+           'ko',
+           markersize=8)
 
-DS.set(pars=pre['parameters'],
-       ics=pre['inconds'])
-P = sim.get_ss(DS)
+# Plot time course data
+experiments = [{'pre':{'parameters':{'Carbon':1.0,
+                                     'ATP':1.0,
+                                     'Glutamine_ext':1.0},
+                       'inconds':{}},
+                'post':{'parameters':{'Carbon':0.0,
+                                      'ATP':0.0,
+                                      'Glutamine_ext':1.0},
+                        'inconds':{}}},
+               {'pre':{'parameters':{'Carbon':0.0,
+                                     'ATP':0.0,
+                                     'Glutamine_ext':1.0},
+                       'inconds':{}},
+                'post':{'parameters':{'Carbon':1.0,
+                                      'ATP':1.0,
+                                      'Glutamine_ext':1.0},
+                        'inconds':{}}}
+]
 
-DS.set(pars=post['parameters'],
-       ics=P,
-       tdata=[0,30])
-P = sim.simulateModel(DS)
-ax[0].plot(P['t'], P['Sch9'],'k--',lw=2,label='Snf1 -| TORC1')
+plotsettings = [{'style':'k--','label':'Model 1'}, # 'Snf1 -| TORC1'
+                {'style':'#bbbbbb','label':'Model 2'}] #'no interaction'
 
-#####
-DS_mut.set(pars=pre['parameters'],
-       ics=pre['inconds'])
-P = sim.get_ss(DS_mut)
-DS_mut.set(pars=post['parameters'],
-       ics=P,
-       tdata=[0,30])
-P = sim.simulateModel(DS_mut)
-ax[0].plot(P['t'], P['Sch9'],'#777777',lw=2,label='no interaction')
-ax[0].plot([0.0,2.5,5.0,15.0,30.0], [1.0,0.34,0.13,0.15,0.20],'k.',markersize=8)
+MODELPATH = 'data/2018-9-26-12-3-no-sigma/'
 
-####################
-del DS_mut
-del DS
-pre = {'parameters':{'Carbon':0.0,'ATP':0.0,'Glutamine_ext':1.0},'inconds':{}}
-post = {'parameters':{'Carbon':1.0,'ATP':1.0,'Glutamine_ext':1.0},'inconds':{}}
-DS = sim.createModelObject(Model)
-DS_mut = sim.createModelObject(Model)
-DS_mut.set(pars={'w_torc_snf':0.0})
+for exp, axis in zip(experiments, ax):
+    # For each experiment, create a wt  and mutant object,
+    # where the mutant strain lacks the TORC1-Snf1 interaction.
+    # Simulate the shift defined in the experiment, plot
+    # the time courses
+    wt = get_simulator(modelpath=MODELPATH,
+                      SIMULATOR = 'cpp',
+                      SIMARGS = {'executable':'robust.o'})
 
-DS.set(pars=pre['parameters'],
-       ics=pre['inconds'])
-P = sim.get_ss(DS)
+    mut = get_simulator(modelpath=MODELPATH,
+                        SIMULATOR = 'cpp',
+                        SIMARGS = {'executable':'robust.o'})
+    mut.set_attr(pars={'w_torc_snf':0.0})
+    strains = [wt, mut]
+    for model, setting in zip(strains, plotsettings):
+        # For both models, set the preshift parameters and ics
+        model.set_attr(pars=exp['pre']['parameters'],
+                       ics=exp['pre']['inconds'])
+        ss = model.get_ss()
+        model.set_attr(pars=exp['post']['parameters'],
+                       ics=model.get_ss(),
+                       tdata=[0,30])
+        traj = model.simulate_and_get_points()
+        axis.plot(traj['t'], traj['Sch9'],
+                  setting['style'],
+                  label=setting['label'],
+                  lw=LW)
+        
 
-DS.set(pars=post['parameters'],
-       ics=P,
-       tdata=[0,30])
-P = sim.simulateModel(DS)
-ax[1].plot(P['t'], P['Sch9'],'k--',lw=2)
 
-#####
-DS_mut.set(pars=pre['parameters'],
-       ics=pre['inconds'])
-P = sim.get_ss(DS_mut)
-DS_mut.set(pars=post['parameters'],
-       ics=P,
-       tdata=[0,30])
-P = sim.simulateModel(DS_mut)
-ax[1].plot(P['t'], P['Sch9'],'#777777',lw=2)
-ax[1].plot([0.0,2.0,5.0,15.0,30.0],[0.20,0.63,0.92,0.91,1.0],'k.',markersize=8)
-
-ax[0].set_ylabel('%Sch9-P')
+ax[0].set_ylabel('Sch9-P')
 ax[0].legend()
-ax[1].set_ylabel('%Sch9-P')
-#ax[0].xlabel('min')
+ax[0].set_xlabel('time (min)')
 ax[0].set_title('Acute Glucose Starvation')
-ax[1].set_xlabel('min')
+ax[1].set_xlabel('time (min)')
+ax[1].set_yticks([])
 ax[1].set_title('Relief from Glucose Starvation')
-plt.suptitle('Snf1 mediated TORC1 inhibition')
-#plt.tight_layout()
-#plt.show()
-plt.savefig(HOME + '/group/amogh-jalihal/papers/Nutrient-Signaling-Model/figs/Snf1-TORC1.png', dpi=300)
-#return './figs/Snf1-TORC1.png'
+
+#plt.suptitle('Snf1 mediated TORC1 inhibition')
+outpath = 'img/Snf1-TORC1.png'
+plt.tight_layout()
+plt.savefig(outpath, dpi=300)
+
